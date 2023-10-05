96081603275005fac183f3a8a9f5e23ace34724a
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-05 00:25:59 -0700

    changes header h1 text and style  color

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header (){
+    return (
+
+        <h1 style = {{color : '#e42c00'}}>🔥 Bring the Heat Sweets 🔥</h1>
+
+
+
+        )
+}
+
+export default Header
