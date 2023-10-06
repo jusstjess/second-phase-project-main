2eeb0798dcc50e3049774462091fef18d6b96304
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-05 22:43:23 -0700

    Feat : Adds NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
-function NavBar (){
-    return (
-        
-    )
+function NavBar() {
+  return (
+
+
+
+    <Nav class="navbar navbar-dark bg-primary" id="navBarLinks" variant="underline">
+       
+
+
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Shop Now</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled">
+          Contact Us
+        </Nav.Link>
+      </Nav.Item>
+       <Nav.Item>
+        <Nav.Link eventKey="disabled">
+          Cart 
+        </Nav.Link>
+      </Nav.Item>
+       
+
+    </Nav>
+
+  );
 }
+
+export default NavBar;
