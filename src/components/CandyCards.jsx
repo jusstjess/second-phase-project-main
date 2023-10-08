bb80f3182a65c166474f203bc0199834b5065a8e
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 02:36:24 -0700

    updates the add to cart button text & color, fixed

--- src/components/CandyCards.jsx
+++ src/components/CandyCards.jsx
@@ -5,21 +5,21 @@ import Button from 'react-bootstrap/Button';
 import '../index.css'
 
 
-function CandyCards({cardName, cardPicture, cardPrice, cardInventory}) {
+function CandyCards({cardName, cardPicture, cardPrice,btnColor, btnText}) {
   return (
-    <Row xs={1} md={4} className="g-4">
+    <Row xs={1} md={3} className="g-4">
       {Array.from({ length: 1 }).map((_, idx) => (
         <Col key={idx}>
           <Card className='card-image' >
-            <Card.Img variant="top" src={cardPicture} />
-            <Card.Body>
+            <Card.Img id='cardImage' variant="top" src={cardPicture} />
+            <Card.Body id= 'cardBody'>
               <Card.Title id='cardTitle' style={{textAlign:'center' , paddingBottom: '-30'}}>
-              <p style = {{fontSize: 15}} >{cardName}</p>
+              <p style = {{fontSize: 10}} >{cardName}</p>
               </Card.Title>
               <Card.Text style={{paddingTop: '-30px'}}>
-              <p style={{fontSize: 13}}>
+              <p style={{fontSize: 10}}>
               {cardPrice}
-              <Button >{cardInventory}</Button>
+              <Button className='addToCartButton' style={btnColor}>{btnText}</Button>
               </p>
              </Card.Text>
             </Card.Body>
