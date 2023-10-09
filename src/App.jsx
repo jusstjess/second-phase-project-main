e18cfa06f8e1cfef61e976311a86c1ed57e1b04e
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 21:11:58 -0700

    adds routes for navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
-import Button from 'react-bootstrap/Button';
 import './App.css'
 import CandyCarousel from './components/CandyCarousel';
+import CandyCards from "./components/CandyCards";
 import './index.css'
 import NavBar from './components/NavBar';
-import Candies from './components/Candies';
+import CandiesList from "./components/pages/CandiesList";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./components/pages/Cart";
+import ContactUs from "./components/pages/ContactUs";
+import Home from "./components/pages/Home";
+import ShopNow from "./components/pages/ShopNow";
+import { Carousel } from "react-bootstrap";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    <NavBar />
-    <Header />
-    <CandyCarousel/>
-    <Candies />
-    {/* <h1 style = {{color:'#FF4E0D'}}>Orange</h1> */}
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
-    {/* <Button style={{color:'yellow'}}>New Button </Button>
-
-       */}
-    </>
+  
+     <>     
+     <NavBar />
+     <Header /> 
+     <Carousel/>
+     <div>
+      <Routes>  
+        <Route path = '/' element ={<Home/>}/>
+        <Route path = "/ShopNow" element = {<ShopNow/>}/>
+        <Route path = "/Cart" element ={<Cart/>}/>
+        <Route path = "/ContactUs" element ={<ContactUs/>} />
+      </Routes>
+      </div> 
+      </>
   )
 }
 
