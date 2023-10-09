db1916e06680591e39252448a07f9e2a14357638
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 21:04:49 -0700

    moved file down to components

---
+++ src/components/CandyCards.jsx
@@ -0,0 +1,28 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import '../index.css'
+
+
+
+function CandyCards({cardName, cardPicture, cardPrice,btnColor, btnText}) {
+  return (
+   
+          <Card  border="black" className='card-image' >
+            <Card.Img id='cardImage' variant="top" src={cardPicture} />
+            <Card.Body id= 'cardBody'>
+              <Card.Title id='cardTitle' style={{textAlign:'center' , paddingBottom: '-30'}}>
+              <p style = {{fontSize: 10}} >{cardName}</p>
+              </Card.Title>
+              <Card.Text style={{paddingTop: '-30px'}}>
+              <p style={{fontSize: 10}}>
+              {cardPrice}
+              <Button className='addToCartButton' style={btnColor}>{btnText}</Button>
+              </p>
+             </Card.Text>
+            </Card.Body>
+          </Card>
+       
+  );
+}
+
+export default CandyCards;
