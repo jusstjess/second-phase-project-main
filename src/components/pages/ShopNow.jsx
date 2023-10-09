0219608d7d98e176baf9eb8e382c3be6442fa74f
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 21:04:17 -0700

    Adds our shopNow component for our navBar

---
+++ src/components/pages/ShopNow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CandiesList from "./CandiesList";
+
+function ShopNow(){
+    return (
+        <div id ="candyCards">
+        <CandiesList />
+        <br/>
+        </div>
+
+
+    )
+}
+
+export default ShopNow
