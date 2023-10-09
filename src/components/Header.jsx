4537d8829a2fbcb7b0252c9b8dcb376c4a6c0ff5
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 18:26:29 -0700

    updates the Header text content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 function Header (){
     return (
-
-        <h2 style = {{color : '#e42c00'}}> Bring the Heat <br/> Sweets </h2>
-
-
-
+        <h2 style = {{color : '#e42c00'}}> Bring the Heat Sweets </h2>
         )
 }
 
