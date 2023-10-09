cd0bf46cb08269a7751c3ffa7d047d1c4c0a674b
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 19:46:57 -0700

    creates home component for router

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CandyCarousel from "../CandyCarousel";
+
+function Home (){
+    return(
+        <>
+        <CandyCarousel/>
+        </>
+    )
+}
+export default Home;
