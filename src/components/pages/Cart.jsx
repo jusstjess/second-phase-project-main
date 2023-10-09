f7e4c9eb28c47f94f621bf9327e3c103b3462d4e
Jess Leyva <jessica.leyvat@yahoo.com>
2023-10-08 18:26:53 -0700

    creates Cart component

---
+++ src/components/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Cart (){
+
+    return(
+        <>
+
+        <h1>Welcome to your cart.</h1><br/>
+        <h3>Your item total is </h3>
+        
+        </>
+    )
+}
+
+export default Cart;
